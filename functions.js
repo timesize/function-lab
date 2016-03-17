09801b26f7afaba38faf8691392a448ccc824801
Nathan L <adamnate@gmail.com>
2016-03-16 18:56:02 -0700

    isPrime filter hmwk

--- functions.js
+++ functions.js
@@ -1,1 +1,45 @@
-// YOUR CODE HERE
+var sillySum = function(arr) {
+  var total = 0;
+  for (var i = 0; i < arr.length; i ++) {
+    total += arr[i] * i;
+  }
+  return total;
+};
+
+var myArray = [1, 2, 3];
+console.log(sillySum(myArray));
+
+
+//isPrime
+var isPrime = function(num) {
+  if (num < 2) {
+    return false;
+  }
+  for (var i = 2; i < num; i ++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(isPrime(8));
+
+
+//filter
+var filter = function(arr, callback) {
+  var newArr = [];
+    for(var i=0; i<arr.length; i++) {
+      if (callback(arr[i])){
+        newArr.push(arr[i]);
+      }
+    }
+  return newArr;
+}
+
+var isOdd = function(num) {
+	return num % 2 !== 0;
+};
+
+filter ([0,1,2,3,4,5], isOdd);
+// returns [1, 3, 5]
